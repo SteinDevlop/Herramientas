01c95335ed58909901a7bb265c017d92e5febc58
Alejandro Steinman <steinman920@gmail.com>
2022-12-04 20:53:37 -0500

    Playlist complete

--- Descargar video de YT en mp3/Ejecutable.py
+++ Descargar video de YT en mp3/Ejecutable.py
@@ -1,5 +1,4 @@
 from comandos import commands as cm
-from url import urls as url
 
 def eleccion_2_mensaje():
     print("""1. Añadir un video.
@@ -9,21 +8,20 @@ def eleccion_2_mensaje():
 
 
 eleccion_tipo = None
-while eleccion_tipo != 5:
+while eleccion_tipo != 4:
     print("".center(50,"-"))
     print("Descargar video de yt en mp3")
     print("".center(50,"-"))
     print("""
-    Advertencia : **********""")
+    Advertencia : No me hago responsable por el uso malintencionado del programa, es utilizado con propositos educativos.""")
     print("".center(50,"-"))
     print("""1. solo un link.
-2. Gestionar lista de links. (varios que no estan en una playlist)
-3. Playlist.
-4. Borrar musica descargada y resetear lista de descarga.
-5. Salir.""")
+2. Playlist.
+3. Borrar musica descargada y resetear lista de descarga.
+4. Salir.""")
     print("".center(50,"-"))
     try:
-        eleccion_tipo = int(input("(1/2/3/4/5): "))
+        eleccion_tipo = int(input("(1/2/3/4: "))
     except Exception as e:
         print(f"Error generado tipo: {type(e)}")
     if eleccion_tipo == 1:
@@ -32,13 +30,14 @@ while eleccion_tipo != 5:
         cm.one_link(link)
         print("".center(50,"-"))
     elif eleccion_tipo == 2:
-        pass
+        print("".center(50,"-"))
+        link = (str(input("Url de la playlist YT: ")))
+        cm.playlist(link)
+        print("".center(50,"-"))
     elif eleccion_tipo == 3:
-        pass
-    elif eleccion_tipo == 4:
         print("".center(50,"-"))
         cm.delete_data()
         print("Datos borrados")
         print("".center(50,"-"))
-    elif eleccion_tipo == 5:
+    elif eleccion_tipo == 4:
         exit()
--- Descargar video de YT en mp3/comandos.py
+++ Descargar video de YT en mp3/comandos.py
@@ -1,8 +1,11 @@
+from pytube import Playlist
 from pytube import YouTube
+import moviepy.editor as mp
 import os, shutil,time
+import re
 class commands:
-    path_list = "lista amplia.txt"
-    path_dowload = "\Download"
+    path_dowload = "Descargar video de YT en mp3\Download"
+    path_playlist = "Descargar video de YT en mp3\download playlist"
     def __init__(self) -> None:
         pass
     @classmethod
@@ -16,48 +19,67 @@ class commands:
         video_length = video.length
         video_author = video.author
         print("Proceso de descarga".center(50,"-"))
+        print("Informacion del video:")
         print(f"""
-        Informacion del video:
         Titulo: {video.title}.
         Duracion: {video.length} Seg.
         Canal: {video.author}.
         """)
-        print("Audios disponibles".center(50,"-"))
-        filter_video_audio = video.streams.filter(only_audio=True,mime_type="audio/mp4")
-        for st in filter_video_audio:
-            print(st)
         print("".center(50,"-"))
-        #Seleccion de descarga
+        filter_video_audio = video.streams.filter(file_extension='mp4').first()
         try:
-            download_choose = int(input("""(si no quere descargarlo, digite cualquier letra)
-            numero itag: """))
-        except ValueError as e:
-            exit()
-        except Exception as esxc:
-            print(f"Error: {esxc}")
-        try:
-            stream = video.streams.get_by_itag(download_choose)
-        except AttributeError:
-            print("Itag seleccionado no es valido.")
-        except Exception as e:
-            print(f"Error: {type(e)}")
-        try:
-            stream.download(cls.path_dowload)
+            video_mp4=filter_video_audio.download(cls.path_dowload)
         except Exception as e:
             print(f"Error: {type(e)}")
         print("""¡Descarga Finalizada!""")
-        time.sleep(10)
-    def multiple_links(url):
-        pass
-        #Descarga varios videos en .mp3 y lo guarda en una carpeta en download
-
-
-    def playlist(url):
-        pass
+        print("".center(50,"-"))
+        print("Iniciando conversion de mp4 a mp3 ...")
+        time.sleep(5)
+        basePath, extension = os.path.splitext(video_mp4)
+        with mp.VideoFileClip(os.path.join(basePath + ".mp4")) as clip:
+            clip.audio.write_audiofile(os.path.join(basePath + ".mp3"))
+        print("".center(50,"-"))
+        print("¡Conversion finalizada!")
+        print("".center(50,"-"))
+        os.remove(os.path.join(basePath + ".mp4"))
+        print("Proceso Finalizado")
+        print("Volviendo al panel de opciones ...")
+        print("".center(50,"-"))
+        time.sleep(5)
+    @classmethod
+    def playlist(cls,url):
+        video_playlist=Playlist(url)
+        print("Proceso de descarga".center(50,"-"))
+        print("Informacion del video:")
+        print(f"""
+        Titulo: {video_playlist.title}.
+        """)
+        print("".center(50,"-"))
+        confirm = str(input("Confirme la descarga con y (diferente de y, cancelara la descarga): "))
+        if confirm == "y":
+            print("".center(50,"-"))
+            print("Iniciando Descarga ...")
+            for url in video_playlist:
+                YouTube(url).streams.filter(only_audio=True).first().download(cls.path_playlist)
+            print("¡Descarga finalizada!")
+            print("".center(50,"-"))
+            print("Iniciando conversion ...")
+            for file in os.listdir(cls.path_playlist):
+                if re.search('mp4', file):
+                    mp4_path = os.path.join(cls.path_playlist,file)
+                    mp3_path = os.path.join(cls.path_playlist,os.path.splitext(file)[0]+'.mp3')
+                    new_file = mp.AudioFileClip(mp4_path)
+                    new_file.write_audiofile(mp3_path)
+                    os.remove(mp4_path)
+            print("Proceso Finalizado")
+            print("Volviendo al panel de opciones ...")
+            print("".center(50,"-"))
+            time.sleep(5)
+        elif confirm != "y":
+            pass
     
     @classmethod
     def delete_data(cls):
-        #Borrar el contenido de la carpeta download
         for filename in os.listdir(cls.path_dowload):
             file_path = os.path.join(cls.path_dowload, filename)
         try:
@@ -66,7 +88,13 @@ class commands:
             elif os.path.isdir(file_path):
                 shutil.rmtree(file_path)
         except Exception as e:
-            print('Failed to delete %s. Reason: %s' % (file_path, e))
-        #limpiar el contenido del archivo .txt
-        with open(cls.path_list,"w",encoding="utf8"):
-            pass
+            print(f"Fallo Code: {e}")
+        for filename in os.listdir(cls.path_playlist):
+            file_path = os.path.join(cls.path_playlist, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print(f"Fallo Code: {e}")
